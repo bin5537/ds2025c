02172e0f98af38e616bc96311f87e5e5e95d6032
Park Subin <subinp5537@gmail.com>
2025-03-17 15:06:41 +0900

    Update week02.py (v0.2)

--- week02.py
+++ week02.py
@@ -1,8 +1,10 @@
 n = int(input("정수 입력: "))
-result = 0
 
-for i in range(1, n+1):
-    result = result + i
+# result = 0
+# for i in range(1, n+1):
+#     result = result + i
+
+result = n * (n + 1) // 2 # 0(1)
 
 print(result)
 
