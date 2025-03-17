76ddc00255aa667435bf2a55d274330eca926ced
Park Subin <subinp5537@gmail.com>
2025-03-17 15:37:07 +0900

    Update week02.py

--- week02.py
+++ week02.py
@@ -4,7 +4,10 @@ n_random = random.randrange(1, 100)
 win = False
 
 for i in range(7):
-    print(f"{7 - i} 번의 기회가 남았습니다. 숫자 입력: ", end = "")
+    if (7 - i) != 1:
+        print(f"{7 - i} 번의 기회가 남았습니다. 숫자 입력: ", end="")
+    else:
+        print("마지막 기회 입니다. 숫자 입력: ", end="")
     answer = int(input(""))
 
     if n_random == answer:
@@ -16,6 +19,6 @@ for i in range(7):
         print("입력하신 수는 정답보다 큰 수 입니다.")
 
 if win:
-    print("정답입니다!")
+    print("You Win!")
 else:
-    print("정답을 맞추지 못하였습니다!")
+    print(f"You Lose, The answer is {n_random}")
