aa436e0813bd3cda204b99e93f63c01ac4c59b7d
Park Subin <subinp5537@gmail.com>
2025-03-24 15:18:22 +0900

    Update week03-4.py (집합 관련 추가 방법)

--- week03/week03-4.py
+++ week03/week03-4.py
@@ -2,7 +2,10 @@ def inters(l1, l2):
     s1 = set(l1)
     s2 = set(l2)
     # return sorted(list(s1.intersection((s2))))
+
+    # return  sorted(list(s1 | s2))
     return  sorted(list(s1 & s2))
+    # return  sorted(list(s1 - s2))
 
 l1 = [45, 5, 22, 31, 7, 19]
 l2 = [2, 1, 5, 22, 7, 38, 28, 13, 41, 19]
