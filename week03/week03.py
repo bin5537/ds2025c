a51091031bfb471d3ba823d84cd6efb0117ecfa7
Park Subin <subinp5537@gmail.com>
2025-03-24 14:25:09 +0900

    Update week03.py v0.2

--- week03/week03.py
+++ week03/week03.py
@@ -1,3 +1,18 @@
-l = [99, 0, -7, 0, 16]
-for i in range(len(l)):
-    print(f"{l[i]:2} {id(l[i])}")
+import array
+
+def move_zeros(arr):
+    zero_index = 0
+    for index in range(len(arr)):
+        n = arr[index]
+        if n != 0:
+            arr[zero_index] = n
+            if zero_index != index:
+                arr[index] = 0
+            zero_index += 1
+    return(arr)
+
+arr = [99, 0, -7, 0, 16]
+# arr = array.array("i", [99, 0, -7, 0, 16])
+
+move_zeros(arr)
+print(arr)
