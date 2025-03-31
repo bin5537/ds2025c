40fa256a63f597477c303fe071ec4cc16804bb0d
Park Subin <subinp5537@gmail.com>
2025-03-31 15:02:20 +0900

    Update week04.py
    기존 검색 기능에서 음수는 검색이 안됨.
    음수또한 검색이 가능하도록 while문을 current.link에서 current로 변경

--- week04/week04.py
+++ week04/week04.py
@@ -15,16 +15,16 @@ class LinkedList:
         while current.link:
             current = current.link
         current.link = Node(data)
-        
+
     def search(self, target):
         current = self.head
-        while current.link:
+        while current:
             if current.data == target:
                 return f"{target}을(를) 찾았습니다."
             else:
                 current = current.link
         return f"{target}은(는) 링크드 리스트에 존재하지 않습니다!"
-                
+
 
     def __str__(self):
         current = self.head
