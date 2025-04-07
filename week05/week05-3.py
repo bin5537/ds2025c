10c96c1eacd595ffb101184c34f885aa62e65460
Park Subin <subinp5537@gmail.com>
2025-04-07 15:13:59 +0900

    Create week05-3.py

---
+++ week05/week05-3.py
@@ -0,0 +1,16 @@
+def check_parentheses(expression: str) -> bool:
+    stack = []
+
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+print(check_parentheses("(2+3)"))
+print(check_parentheses("(2+(3*9))"))
+print(check_parentheses("(2+(3*9)"))
