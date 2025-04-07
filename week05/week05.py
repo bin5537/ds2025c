a52e7c62001ff44be0a95dbb1e3c7469908b5139
Park Subin <subinp5537@gmail.com>
2025-04-07 14:56:36 +0900

    Create week05.py

---
+++ week05/week05.py
@@ -0,0 +1,13 @@
+s1 = list()
+print(len(s1))
+
+s1.append("Data structure")     # push
+s1.append("Database")           # push
+
+print(len(s1))          # size
+print(s1[-1])           # peek
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
