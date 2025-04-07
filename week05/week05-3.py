71c845848213c7a2ec130f25c5f0694b18accc45
Park Subin <subinp5537@gmail.com>
2025-04-07 15:44:54 +0900

    Update week05-3.py

--- week05/week05-3.py
+++ week05/week05-3.py
@@ -1,16 +1,21 @@
-def check_parentheses(expression: str) -> bool:
+def is_vaild_brackets(expression: str) -> bool:
     stack = []
 
-    for letter in expression:
-        if letter == "(":
-            stack.append(letter)
-        if letter == ")":
-            if len(stack) == 0:
-                return False
-            else:
-                stack.pop()
-    return len(stack) == 0
+    brackets = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    }
 
-print(check_parentheses("(2+3)"))
-print(check_parentheses("(2+(3*9))"))
-print(check_parentheses("(2+(3*9)"))
+    for letter in expression:
+        if letter in brackets.values():
+            stack.append(letter)
+        elif letter in brackets.keys():
+            if not stack or brackets[letter] != stack.pop():
+                return False
+    return not stack
+
+print(is_vaild_brackets("(2+3)"))
+print(is_vaild_brackets("(2+(3*9))"))
+print(is_vaild_brackets("(2+{4/5])"))
+print(is_vaild_brackets("[2 + {3 * 5(5+5)}]"))
