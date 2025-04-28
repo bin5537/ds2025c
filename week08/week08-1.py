6a538a45ba602bf50acbd6204b0bc5ebbb4292a6
Park Subin <subinp5537@gmail.com>
2025-04-28 15:13:55 +0900

    Create week08-1.py

---
+++ week08/week08-1.py
@@ -0,0 +1,39 @@
+class TreeNode:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
+
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.data, end = " - ")
+
+if __name__ == "__main__":
+    numbers = [10, 15, 8, 3, 9]
+    root = None
+
+    node = TreeNode()
+    node.data = numbers[0]
+    root = node
+
+    # 2번째 원소 부터 마지막 원소까지
+    for number in numbers[1:]:
+        newNode = TreeNode()
+        newNode.data = number
+        current = root
+        while True:
+            if number < current.data:
+                if current.left is None:
+                    current.left = newNode
+                    break
+                current = current.left
+            else:
+                if current.right is None:
+                    current.right = newNode
+                    break
+                current = current.right
+    
+    print("BST 구성 완료")
+    post_order(root)
