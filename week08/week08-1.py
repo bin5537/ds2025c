e247da6d098965e6e6155dee4b0910cbbd1d36ec
Park Subin <subinp5537@gmail.com>
2025-05-12 13:59:02 +0900

    Update week08-1.py

--- week08/week08-1.py
+++ week08/week08-1.py
@@ -4,11 +4,13 @@ class TreeNode:
         self.right = None
         self.data = None
 
+
 def post_order(node):
     if node:
         post_order(node.left)
         post_order(node.right)
-        print(node.data, end = "->")
+        print(node.data, end="->")
+
 
 def insert(root, value):
     newNode = TreeNode()
@@ -31,18 +33,21 @@ def insert(root, value):
             current = current.right
     return root
 
-def search(root, value):
+
+def search(find_number):
     current = root
-    while current:
-        if value == current.data:
+    while True:
+        if find_number == current.data:
             return True
-        elif value < current.data:
-            return False
+        elif find_number < current.data:
+            if current.left is None:
+                return False
+            current = current.left
         else:
             if current.right is None:
                 return False
-            current = current.data
-    return False
+            current = current.right
+
 
 if __name__ == "__main__":
     numbers = [10, 15, 8, 3, 9, 1, 7, 100, 20]
@@ -53,12 +58,10 @@ if __name__ == "__main__":
         root = insert(root, number)
 
     print("BST 구성 완료")
-    post_order(root)    # 3->9->8->15->10
+    post_order(root)  # 3->9->8->15->10
 
     find_number = int(input())
-    
-    if search(root, find_number):
-        print(f"{find_number}을(를) 찾았습니다.")
+    if search(find_number):
+        print(f"{find_number}을(를) 찾았습니다")
     else:
-        print(f"{find_number}을(를) 못찾았습니다.")
-        
+        print(f"{find_number}이(가) 존재하지 않습니다")
